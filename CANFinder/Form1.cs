a0561e87761c1b4b47115922afe9b3d2bdafb08f
ZachM <zmorris@merrick-inc.com>
2021-04-14 13:13:55 -0500

    gearmode as number
    gearmode as number to play better with dashcam.

--- CANFinder/Form1.cs
+++ CANFinder/Form1.cs
@@ -444,7 +444,7 @@ namespace CANFinder
                     timestamp,
                     Soc,
                     Amps,
-                    GMString,
+                    GearMode,
                     Odo,
                     Trip,
                     Speed,
